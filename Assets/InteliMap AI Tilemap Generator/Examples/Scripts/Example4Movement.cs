c3f518c06a5ebb83c8f09a3c98ce64d8c53b7c80
Kush8899 <kushagranagar25@gmail.com>
2023-07-21 00:18:26 +0530

    Random level generation

--- Assets/InteliMap AI Tilemap Generator/Examples/Scripts/Example4Movement.cs
+++ Assets/InteliMap AI Tilemap Generator/Examples/Scripts/Example4Movement.cs
@@ -8,11 +8,11 @@ namespace InteliMap
     {
         public Transform mainCamera;
         public InteliMapGenerator bottomSchematic;
-        public InteliMapGenerator topSchematic;
+        //public InteliMapGenerator topSchematic;
         public float speed = 2.0f;
         public int chunkWidth = 10;
         public int chunkOverlap = 2;
-
+        public int offset = 0;
         private int lastChunkLocation = 0;
         private float cameraStart;
 
@@ -22,24 +22,24 @@ namespace InteliMap
 
             // Set the x size to be equal to the chunk width
             bottomSchematic.boundsToFill.size = new Vector3Int(chunkWidth, bottomSchematic.boundsToFill.size.y, 1);
-            topSchematic.boundsToFill.size = new Vector3Int(chunkWidth, topSchematic.boundsToFill.size.y, 1);
+            //topSchematic.boundsToFill.size = new Vector3Int(chunkWidth, topSchematic.boundsToFill.size.y, 1);
         }
 
         public void Update()
         {
             // Move the main camera forward
-            mainCamera.position = new Vector3(mainCamera.position.x + speed * Time.deltaTime, mainCamera.position.y, mainCamera.position.z);
+            //mainCamera.position = new Vector3(mainCamera.position.x + speed * Time.deltaTime, mainCamera.position.y, mainCamera.position.z);
 
             // If the main camera surpasses some threshold
-            if (mainCamera.position.x - cameraStart > lastChunkLocation)
+            if (mainCamera.position.x - cameraStart > (lastChunkLocation-offset))
             {
                 // Generate the next chunk
-                bottomSchematic.StartGeneration();
-                topSchematic.StartGeneration();
+                bottomSchematic.StartGenerationWithSeed(123123123);
+                //topSchematic.StartGeneration();
 
                 // Move the chunks position for the next iterations
                 bottomSchematic.boundsToFill.position = new Vector3Int(bottomSchematic.boundsToFill.position.x + chunkWidth - chunkOverlap, bottomSchematic.boundsToFill.position.y);
-                topSchematic.boundsToFill.position = new Vector3Int(topSchematic.boundsToFill.position.x + chunkWidth - chunkOverlap, topSchematic.boundsToFill.position.y);
+                //topSchematic.boundsToFill.position = new Vector3Int(topSchematic.boundsToFill.position.x + chunkWidth - chunkOverlap, topSchematic.boundsToFill.position.y);
                 lastChunkLocation += chunkWidth - chunkOverlap;
             }
         }
